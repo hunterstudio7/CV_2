16054750d8678335df6618f49d844a45c68b94cd
Sudakov Kirill <hunterstudio7@gmail.com>
2021-05-09 05:00:51 +0300

    Sv2

---
+++ Assets/Scripts/Buttons.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public Sprite layer_blue, layer_red;
+    public GameObject m_on, m_off;
+    private void Start()
+    {
+        if (gameObject.name == "Music")
+        {
+            if (PlayerPrefs.GetString("Music") == "no")
+            {
+                m_on.SetActive(false);
+                m_off.SetActive(true);
+            }
+            else
+            {
+                m_on.SetActive(true);
+                m_off.SetActive(false);
+            }
+        }
+    }
+
+    void OnMouseDown()
+    {
+        GetComponent <SpriteRenderer> ().sprite = layer_red;
+    }
+    void OnMouseUp()
+    {
+        GetComponent<SpriteRenderer>().sprite = layer_blue;
+    }
+    void OnMouseUpAsButton()
+    {
+        if (PlayerPrefs.GetString("Music") != "no")
+        {
+            GameObject.Find("Click Audio").GetComponent<AudioSource>().Play();
+        }
+        switch(gameObject.name){
+            case "Play":
+                Application.LoadLevel("play");
+                break;
+            case "Share":
+                Application.OpenURL("http://instagram.com/hunterstudio7");
+                break;
+
+            case "Replay":
+                Application.LoadLevel("play");
+                break;
+            case "Home":
+                Application.LoadLevel("main");
+                break;
+            case "help":
+                Application.LoadLevel("howTo");
+                break;
+            case "Close":
+                Application.LoadLevel("main");
+                break;
+            case "Music":
+                if (PlayerPrefs.GetString("Music") != "no")
+                {
+                    PlayerPrefs.SetString("Music", "no");
+                    m_on.SetActive(false);
+                    m_off.SetActive(true);
+                }
+                else {
+                    PlayerPrefs.SetString("Music", "yes");
+                    m_on.SetActive(true);
+                    m_off.SetActive(false);
+                }
+                break;
+
+        }
+    }
+}
---
+++ Assets/Scripts/CubeFloat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeFloat : MonoBehaviour {
+
+    public float speed, tilt;
+    public Vector3 target;
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
+        if (transform.position == target && target.y != 0.1f)
+            target.y = 0.1f;
+        else if (transform.position == target && target.y == 0.1f)
+            target.y = 1.5f;
+        transform.Rotate(Vector3.up * Time.deltaTime * tilt);
+    }
+}
